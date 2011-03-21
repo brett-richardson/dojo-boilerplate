6d44e11344b0a7b88a1d362947366286fff4d460
Rebecca Murphey <rebecca@rebeccamurphey.com>
2011-03-20 23:51:51 -0400

    renaming

---
+++ www/js/app/base.js
@@ -0,0 +1,11 @@
+/**
+ * This file is your application's base JavaScript file;
+ * it is loaded into the page by the dojo.require() call in
+ * index.html. You can write code in this file, use it to
+ * express dependencies on other files, or both.
+ */
+dojo.provide("app.base");
+
+dojo.addOnLoad(function() {
+
+});
