524864a0bdc55e31bb1e91b76ec13749d8af53fe
Brett Richardson <brett.richardson.nz@gmail.com>
2012-10-05 04:29:23 +0200

    Started re-fixing goal manager

--- profiles/cocoach.profile.js
+++ profiles/cocoach.profile.js
@@ -32,10 +32,10 @@ var profile = {
 	// Uses Closure Compiler as the JavaScript minifier. This can also be set to "shrinksafe" to use ShrinkSafe.
 	// Note that you will probably get some “errors” with CC; these are generally safe to ignore, and will be
 	// fixed in a later version of Dojo. This defaults to "" (no compression) if not provided.
-	optimize: 'closure',
+	optimize: '', // 'closure',
 
 	// We’re building layers, so we need to set the minifier to use for those, too. This defaults to "shrinksafe" if it is not provided.
-	layerOptimize: 'closure',
+	layerOptimize: '', // 'closure',
 
 	// Strips all calls to console functions within the code. You can also set this to "warn" to strip everything
 	// but console.error, and any other truthy value to strip everything but console.warn and console.error.
@@ -45,7 +45,7 @@ var profile = {
 	// smaller. We add it back here to avoid that extra HTTP request. There is also a "lite" selector available; if
 	// you use that, you’ll need to set selectorEngine in app/run.js too. (The "lite" engine is only suitable if you
 	// are not supporting IE7 and earlier.)
-	selectorEngine: 'acme',
+	selectorEngine: 'lite',  // 'acme', // FIXME: May need to add this back in for IE
 
 	// Builds can be split into multiple different JavaScript files called “layers”. This allows applications to
 	// defer loading large sections of code until they are actually required while still allowing multiple modules to
@@ -62,30 +62,30 @@ var profile = {
 			include: [
 				// 'dojo/i18n',
 				'dojo/main',
-				'dojo/domReady',
+				'dojo/_base/declare',
+				'dojo/_base/lang',
+				'dojo/_base/connect',
 				'dojo/require',
 				'dojo/provide',
-				'dojo/_base/declare',
-				'dojo/_base/connect',
-				'dojo/_base/declare'
+				'dojo/domReady',
+				'dojo/Deferred',
+				'dojo/when',
+				'dojo/on',
+				'dojo/store/Observable',
+				'dojo/store/JsonRest'
 			],
-
 			// By default, the build system will try to include `dojo/main` in the built `dojo/dojo` layer, which adds
 			// a bunch of stuff we do not want or need. We want the initial script load to be as small and quick to
 			// load as possible, so we configure it as a custom, bootable base.
 			boot: true,
 			customBase: true
 		},
-		// This is the main loader module. It is a little special because it is treated like an AMD module even though it is actually just plain JavaScript. There is some extra magic in the build system specifically for this module ID.
 		'app/cocoach_base': {
-			// In addition to the loader (dojo/dojo) and the loader configuration file (app/run), we’re also including
-			// the main application (app/main) and the dojo/i18n and dojo/domReady modules because they are one of the
-			// conditional dependencies in app/main (the other being app/Dialog) but we don’t want to have to make
-			// extra HTTP requests for such tiny files.
 			include: [
-				'dojo/store/JsonRest',
 				'dojox',
+				'dijit',
 				'dijit/Tree',
+				'dijit/tree/ObjectStoreModel',
 				'dijit/tree/dndSource',
 				'dijit/Menu',
 				'dijit/MenuItem',
@@ -94,28 +94,30 @@ var profile = {
 				'dijit/form/Button',
 				'dijit/_editor/_Plugin'
 			],
-
-			dependencies: [
-				"dijit.Menu",
-				"dijit.MenuItem",
-				"dijit.ToolBar",
-				"dijit.Editor",
-				"dijit.form.Button"
+			boot: false,
+			customBase: false // true
+		},
+		'app/cocoach_document_builder': {
+			include: [
+				'dijit/tree/dndSource'
 			],
-
-			// By default, the build system will try to include dojo/main in the built dojo/dojo layer, which adds a
-			// bunch of stuff we don’t want or need. We want the initial script load to be as small and quick as
-			// possible, so we configure it as a custom, bootable base.
-			boot: true,
+			boot: false,
+			customBase: false // true
+		},
+		'app/cocoach_goal_manager': {
+			include: [
+				'dijit/tree/dndSource'
+			],
+			boot: false,
 			customBase: false // true
 		}
 	},
 
 	// Providing hints to the build system allows code to be conditionally removed on a more granular level than simple module dependencies can allow. This is especially useful for creating tiny mobile builds.  Keep in mind that dead code removal only happens in minifiers that support it! Currently, ShrinkSafe does not support dead code removal; Closure Compiler and UglifyJS do.
 	staticHasFeatures: {
-		'dojo-trace-api'        : 0, // The trace & log APIs are used for debugging the loader, so we don’t need them in the build
-		'dojo-log-api'          : 0,
-		'dojo-publish-privates' : 0, // This causes normally private loader data to be exposed for debugging, so we don’t need that either
+		'dojo-trace-api'        : 1, // 0 // The trace & log APIs are used for debugging the loader, so we don’t need them in the build
+		'dojo-log-api'          : 1, // 0,
+		'dojo-publish-privates' : 1, // 0, // This causes normally private loader data to be exposed for debugging, so we don’t need that either
 		'dojo-sync-loader'      : 0, // We’re fully async, so get rid of the legacy loader
 		'dojo-xhr-factory'      : 0, // dojo-xhr-factory relies on dojo-sync-loader
 		'dojo-test-sniff'       : 0 // We aren’t loading tests in production
@@ -123,19 +125,19 @@ var profile = {
 
 	// Resource tags are functions that provide hints to the compiler about a given file. The first argument is the filename of the file, and the second argument is the module ID for the file.
 	resourceTags: {
-		test: function (filename, mid) { // Files that contain test code.
+		test: function( filename, mid ){ // Files that contain test code.
 			return false;
 		},
 
-		copyOnly: function (filename, mid) { // Files that should be copied as-is without being modified by the build system.
-			return copyOnly(mid);
+		copyOnly: function( filename, mid ){ // Files that should be copied as-is without being modified by the build system.
+			return copyOnly( mid );
 		},
 
-		amd: function (filename, mid) { // Files that are AMD modules.
-			return !copyOnly(mid) && /\.js$/.test(filename);
+		amd: function( filename, mid ){ // Files that are AMD modules.
+			return !copyOnly( mid ) && /\.js$/.test( filename );
 		},
 
-		miniExclude: function (filename, mid) { // Files that should not be copied when the “mini” compiler flag is set to true.
+		miniExclude: function( filename, mid ){ // Files that should not be copied when the “mini” compiler flag is set to true.
 			return mid in {
 				'app/profile': 1
 			};
